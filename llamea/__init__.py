94a1940e9d6b1ab8215a0ea6e7f83c2162db69bb
ewenli <>
2025-02-09 05:56:46 +0100

    Load environment variables at module initialization for improved configuration management

--- llamea/__init__.py
+++ llamea/__init__.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+load_dotenv()
+
 from .ast import analyze_run, plot_optimization_graphs, process_code
 from .individual import Individual 
 from .llamea import LLaMEA
@@ -6,6 +8,4 @@ from .llambo import LLaMBO
 from .loggers import ExperimentLogger
 from .utils import NoCodeException
 from .evaluator.injected_critic import AlgorithmCritic
-
-load_dotenv()
 from .llm import LLMmanager, LLMS
