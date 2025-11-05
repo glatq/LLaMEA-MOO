ba661b096aabad9d74f945f3a4da3fc46ca4a395
George Laskaris <137825403+glatq@users.noreply.github.com>
2025-11-05 13:04:49 +0100

    Test pymoo (#9)

---
+++ tests/test_pymoo.py
@@ -0,0 +1,12 @@
+from pymoo.indicators.hv import HV
+from pymoo.problems import get_problem
+import numpy as np
+
+def test_problem_load():
+    problem = get_problem("zdt1")
+    assert problem.name() == "ZDT1"
+def test_hv_calculation():
+    ref_point = np.array([10, 10])
+    f_pareto = np.array([[2,8],[4,4],[8,2]])
+    hv = HV(ref_point=ref_point)
+    assert hv(f_pareto) == 44.0
