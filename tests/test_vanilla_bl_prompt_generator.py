ed838f2748e59dbcdf7de319845230355285938f
George Laskaris <137825403+glatq@users.noreply.github.com>
2026-01-29 18:06:45 +0100

    Separate tests in vanilla_bl_prompt_generator (#44)

--- tests/test_vanilla_bl_prompt_generator.py
+++ tests/test_vanilla_bl_prompt_generator.py
@@ -8,52 +8,61 @@ from .utils_for_tests import normalize_prompt, make_basic_result
 
 
 class TestTaskDescription:
-    def test_task_description(self, vanilla_bl, bl_task_description):
+    def test_initialize_solution_no_bo(self, vanilla_bl, bl_task_description):
         expected = bl_task_description["initialize_solution_no_bo"]
         actual = normalize_prompt(
             vanilla_bl.task_description(GenerationTask.INITIALIZE_SOLUTION)
         )
         assert expected == actual
 
+    def test_fix_errors_no_bo(self, vanilla_bl, bl_task_description):
         expected = bl_task_description["fix_errors_no_bo"]
         actual = normalize_prompt(
             vanilla_bl.task_description(GenerationTask.FIX_ERRORS)
         )
         assert expected == actual
 
+    def test_fix_errors_from_error_no_bo(self, vanilla_bl, bl_task_description):
         expected = bl_task_description["fix_errors_from_error_no_bo"]
         actual = normalize_prompt(
             vanilla_bl.task_description(GenerationTask.FIX_ERRORS_FROM_ERROR)
         )
         assert expected == actual
 
+    def test_optimize_performance_no_bo(self, vanilla_bl, bl_task_description):
         expected = bl_task_description["optimize_performance_no_bo"]
         actual = normalize_prompt(
             vanilla_bl.task_description(GenerationTask.OPTIMIZE_PERFORMANCE)
         )
         assert expected == actual
 
-        vanilla_bl.is_bo = True
-
+    def test_initialize_solution_bo(self, vanilla_bl, bl_task_description):
         expected = bl_task_description["initialize_solution_bo"]
+        vanilla_bl.is_bo = True
         actual = normalize_prompt(
             vanilla_bl.task_description(GenerationTask.INITIALIZE_SOLUTION)
         )
         assert expected == actual
 
+    def test_fix_errors_bo(self, vanilla_bl, bl_task_description):
         expected = bl_task_description["fix_errors_bo"]
+        vanilla_bl.is_bo = True
         actual = normalize_prompt(
             vanilla_bl.task_description(GenerationTask.FIX_ERRORS)
         )
         assert expected == actual
 
+    def test_fix_errors_from_error_bo(self, vanilla_bl, bl_task_description):
         expected = bl_task_description["fix_errors_from_error_bo"]
+        vanilla_bl.is_bo = True
         actual = normalize_prompt(
             vanilla_bl.task_description(GenerationTask.FIX_ERRORS_FROM_ERROR)
         )
         assert expected == actual
 
+    def test_optimize_performance_bo(self, vanilla_bl, bl_task_description):
         expected = bl_task_description["optimize_performance_bo"]
+        vanilla_bl.is_bo = True
         actual = normalize_prompt(
             vanilla_bl.task_description(GenerationTask.OPTIMIZE_PERFORMANCE)
         )
@@ -61,23 +70,26 @@ class TestTaskDescription:
 
 
 class TestResponseFormat:
-    def test_response_format(self, vanilla_bl, bl_response_format):
+    def test_initialize_solution(self, vanilla_bl, bl_response_format):
         expected = bl_response_format["initialize_solution"]
         actual = normalize_prompt(
             vanilla_bl.response_format(GenerationTask.INITIALIZE_SOLUTION)
         )
         assert expected == actual
 
+    def test_fix_errors(self, vanilla_bl, bl_response_format):
         expected = bl_response_format["fix_errors"]
         actual = normalize_prompt(vanilla_bl.response_format(GenerationTask.FIX_ERRORS))
         assert expected == actual
 
+    def test_fix_errors_from_error(self, vanilla_bl, bl_response_format):
         expected = bl_response_format["fix_errors_from_error"]
         actual = normalize_prompt(
             vanilla_bl.response_format(GenerationTask.FIX_ERRORS_FROM_ERROR)
         )
         assert expected == actual
 
+    def test_optimize_performance(self, vanilla_bl, bl_response_format):
         expected = bl_response_format["optimize_performance"]
         actual = normalize_prompt(
             vanilla_bl.response_format(GenerationTask.OPTIMIZE_PERFORMANCE)
@@ -86,19 +98,21 @@ class TestResponseFormat:
 
 
 class TestCodeStructure:
-    def test_code_structure(self, vanilla_bl, bl_code_structure):
+    def test_bo_and_mini_bo(self, vanilla_bl, bl_code_structure):
         vanilla_bl.is_bo = True
         vanilla_bl.use_mini_bo = True
         expected = bl_code_structure["bo_and_mini_bo"]
         actual = vanilla_bl.code_structure()
         assert expected == actual
 
+    def test_bo_and_no_mini_bo(self, vanilla_bl, bl_code_structure):
         vanilla_bl.is_bo = True
         vanilla_bl.use_mini_bo = False
         expected = bl_code_structure["bo_and_no_mini_bo"]
         actual = vanilla_bl.code_structure()
         assert expected == actual
 
+    def test_no_bo_no_mini_bo(self, vanilla_bl, bl_code_structure):
         vanilla_bl.is_bo = False
         vanilla_bl.use_mini_bo = False
         expected = bl_code_structure["no_bo_no_mini_bo"]
@@ -127,19 +141,18 @@ def test_evaluation_feedback_prompt_empty_when_no_results(vanilla_bl, eval_res):
     assert vanilla_bl.evaluation_feedback_prompt(eval_res) == ""
 
 
-def test_get_prompt_with_empty_solution_and_non_INITIALIZE_SOLUTION_task(vanilla_bl):
-    expected_role_prompt, expected_final_prompt = "", ""
-    actual_role_prompt, actual_final_prompt = vanilla_bl.get_prompt(
-        task=GenerationTask.OPTIMIZE_PERFORMANCE,
-        problem_desc="24 noiseless functions",
-        candidates=[],
-    )
-    assert expected_role_prompt == actual_role_prompt
-    assert expected_final_prompt == actual_final_prompt
-
-
 class TestGetPrompt:
-    def test_get_prompt_with_initialize_solution_no_candidates(
+    def test_non_initialize_empty_solution(self, vanilla_bl):
+        expected_role_prompt, expected_final_prompt = "", ""
+        actual_role_prompt, actual_final_prompt = vanilla_bl.get_prompt(
+            task=GenerationTask.OPTIMIZE_PERFORMANCE,
+            problem_desc="24 noiseless functions",
+            candidates=[],
+        )
+        assert expected_role_prompt == actual_role_prompt
+        assert expected_final_prompt == actual_final_prompt
+
+    def test_initialize_solution_no_candidates(
         self, vanilla_bl, vanilla_blrh, bl_get_prompt
     ):
         expected_role_prompt = bl_get_prompt["role_prompt"]
@@ -154,7 +167,7 @@ class TestGetPrompt:
         assert expected_role_prompt == actual_role_prompt
         assert expected_final_prompt == actual_final_prompt
 
-    def test_get_prompt_with_initialize_solution_and_pre_solution_candidates(
+    def test_initialize_solution_with_candidates(
         self, vanilla_bl, vanilla_blrh, bl_get_prompt
     ):
         c1 = vanilla_blrh
@@ -173,7 +186,22 @@ class TestGetPrompt:
         )
         assert expected_final_prompt == actual_final_prompt
 
-    def test_get_prompt_with_non_INITIALIZE_SOLUTION_task(
+    def test_non_initialize_solution(self, vanilla_bl, vanilla_blrh, bl_get_prompt):
+        c1 = vanilla_blrh
+        c1.code_name = "Algorithm 1"
+
+        expected_role_prompt = bl_get_prompt["role_prompt"]
+        expected_final_prompt = bl_get_prompt["non_initialize_solution"]
+        actual_role_prompt, actual_final_prompt = vanilla_bl.get_prompt(
+            task=GenerationTask.FIX_ERRORS_FROM_ERROR,
+            problem_desc="24 noiseless functions",
+            candidates=[c1],
+        )
+
+        assert expected_role_prompt == actual_role_prompt
+        assert expected_final_prompt == normalize_prompt(actual_final_prompt)
+
+    def test_non_initialize_solution_with_crossover(
         self, vanilla_bl, vanilla_blrh, bl_get_prompt
     ):
         c1 = vanilla_blrh
@@ -189,14 +217,6 @@ class TestGetPrompt:
             candidates=[c1, c2],
         )
         assert expected_role_prompt == actual_role_prompt
-        assert expected_final_prompt == normalize_prompt(actual_final_prompt)
-
-        expected_final_prompt = bl_get_prompt["non_initialize_solution"]
-        actual_role_prompt, actual_final_prompt = vanilla_bl.get_prompt(
-            task=GenerationTask.FIX_ERRORS_FROM_ERROR,
-            problem_desc="24 noiseless functions",
-            candidates=[c1],
-        )
         assert expected_final_prompt == normalize_prompt(actual_final_prompt)
 
 
